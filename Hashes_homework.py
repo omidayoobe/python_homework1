130df5121946d621c3bcb5ebd86c296fb88655fb
Omid Ayoobe <oayoobe@spartaglobal.com>
2019-07-15 21:53:31 +0100

    Home work finished

---
+++ Hashes_homework.py
@@ -0,0 +1,25 @@
+# Python Homework Exercise 4
+#
+print("..........Exercise 4..................... ")
+print(' ')
+
+print("Hi there, lets make a story together")
+input('press enter to start your story')
+print(' ')
+
+story = {} #empty hash
+
+story ['Hero'] = input("please type in your favourite hero ") # created a new value and assigned the user input to it
+story ['Beginning'] = input("Now write the beggining of your story ") # created a new value and assigned the user input to it
+story ['Middle'] = input("Now write the middle of your story ") # created a new value and assigned the user input to it
+story ['End'] = input("that sounds interesting, now write an amazing ending ") # created a new value and assigned the user input to it
+
+print(' ')
+input('now press enter to view your amazing story')
+print(' ')
+print("Once there was the Almighty" + ' ' + story['Hero']) # Concatenating th hero with text
+print(story['Beginning']) #print beginning
+print(story['Middle']) #print beginning
+print(story['End']) #print beginning
+
+print (story) #printing the whole has (story)
